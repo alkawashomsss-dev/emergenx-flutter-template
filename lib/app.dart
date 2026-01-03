9b743b5352c4e71bf4fb137c7e1cbb1357be70a6
alkawashomsss-dev <alkawashomsss@gmail.com>
2026-01-03 23:54:52 +0100

    🚀 قائمة المهام: Update lib/app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'تطبيق ملاحظات بسيط',
+      title: 'قائمة المهام',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       home: const HomeScreen(),
