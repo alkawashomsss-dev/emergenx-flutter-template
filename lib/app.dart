5c3679fec36c28a3810369b3a5751f6407907065
alkawashomsss-dev <alkawashomsss@gmail.com>
2026-01-04 00:43:00 +0100

    🚀 سولي: Update lib/app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'تطبيق الأخبار',
+      title: 'سولي',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       home: const HomeScreen(),
