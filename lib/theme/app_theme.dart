bc19542f5d81723a2748d98ed3d47daebfe39825
alkawashomsss-dev <alkawashomsss@gmail.com>
2026-01-03 22:11:48 +0100

    🚀 حاسبة بسيطة: Update lib/theme/app_theme.dart

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -2,15 +2,11 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static final ThemeData lightTheme = ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
     useMaterial3: true,
-    colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
-    appBarTheme: const AppBarTheme(
-      backgroundColor: Colors.teal,
-      foregroundColor: Colors.white,
-    ),
-    floatingActionButtonTheme: const FloatingActionButtonThemeData(
-      backgroundColor: Colors.teal,
-      foregroundColor: Colors.white,
+    textTheme: const TextTheme(
+      headlineLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      bodyMedium: TextStyle(fontSize: 18),
     ),
   );
 }
