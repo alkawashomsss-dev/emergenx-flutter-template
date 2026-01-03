53391cdc4cca091619f4a9b626d75e615ed78ec1
alkawashomsss-dev <alkawashomsss@gmail.com>
2026-01-03 22:00:42 +0100

    🚀 تطبيق ملاحظات: Update lib/theme/app_theme.dart

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -5,6 +5,10 @@ class AppTheme {
     return ThemeData(
       colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
       useMaterial3: true,
+      textTheme: const TextTheme(
+        bodyLarge: TextStyle(fontSize: 18.0),
+        bodyMedium: TextStyle(fontSize: 16.0),
+      ),
     );
   }
 }
