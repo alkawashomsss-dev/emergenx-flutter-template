2855756728d11f0be9d69199776c94c021fbc87f
alkawashomsss-dev <alkawashomsss@gmail.com>
2026-01-04 00:42:54 +0100

    🚀 تطبيق الأخبار: Update lib/theme/app_theme.dart

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static ThemeData get lightTheme {
     return ThemeData(
-      colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
       useMaterial3: true,
     );
   }
