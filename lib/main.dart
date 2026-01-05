30a44edbd2b15228cfc8233d32210fc09bf174bb
alkawashomsss-dev <alkawashomsss@gmail.com>
2026-01-05 02:14:46 +0100

    🚀 لعبة التطابق: Update lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  runApp(const MyGame());
 }
