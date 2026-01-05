e3d00d073f803aec6333011a7965a1bc21e3c3ab
alkawashomsss-dev <alkawashomsss@gmail.com>
2026-01-05 02:14:47 +0100

    🚀 لعبة التطابق: Update lib/app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
-import 'theme/app_theme.dart';
-import 'screens/home_screen.dart';
+import 'screens/menu_screen.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyGame extends StatelessWidget {
+  const MyGame({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'تطبيق قائمة التسوق',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      home: const HomeScreen(),
+      title: 'لعبة التطابق',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MenuScreen(),
     );
   }
 }
