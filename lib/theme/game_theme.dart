1c51651921519117f25776b360830d9381d4d985
alkawashomsss-dev <alkawashomsss@gmail.com>
2026-01-05 02:14:47 +0100

    🚀 لعبة التطابق: Update lib/theme/game_theme.dart

---
+++ lib/theme/game_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class GameTheme {
+  static const Color backgroundColor = Colors.lightBlueAccent;
+  static const Color tileColor = Colors.white;
+  static const Color scoreColor = Colors.black;
+}
